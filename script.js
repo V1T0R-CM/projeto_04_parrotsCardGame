bda22ba4fe7a39d5f3cd07a698bd2717eced68a3
Vitor Carvalho Matos <vitorcarvalhomatos@gmail.com>
2022-04-08 12:26:36 -0300

    Jogo funcional

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const numeroCartas=selecionaNumCartas()
+let permissao=true
+let jogadas=0
+let carta1
+let carta2
+
+function selecionaNumCartas(){
+    let numEscolhido=Number(prompt("Deseja jogar com quantas cartas?\n(4, 6, 8, 10, 12 ou 14)"))
+    while(numEscolhido>14 || numEscolhido<4 || numEscolhido%2!==0){
+        numEscolhido=Number(prompt("Digite um dos números a seguir:\n(4, 6, 8, 10, 12 ou 14)"))
+    }
+    return numEscolhido
+}
+
 function embaralha(lista) {
     let gifSorteado;
     let indiceSorteado;
@@ -11,8 +25,34 @@ function embaralha(lista) {
     return listaEmbaralhada;
   }
 
+function comparaCartas(){
+    const GifCarta1=carta1.querySelector(".face.verso img")
+    const GifCarta2=carta2.querySelector(".face.verso img")
+    if(GifCarta1.getAttribute("src")!==GifCarta2.getAttribute("src")){
+        carta1.classList.remove("flip")
+        carta2.classList.remove("flip")
+    }
+    permissao=true
+    if (document.querySelectorAll(".flip").length===numeroCartas){
+        alert(`Parabéns você acertou todos os pares de cartas em ${jogadas} jogadas!!\n`)
+    }
+}
+
+function flipar(elemento){
+    if(document.querySelectorAll(".flip").length%2===0 && permissao){
+        carta1=elemento
+        elemento.classList.add("flip")
+    }
+    else if(document.querySelectorAll(".flip").length%2!==0 && permissao){
+        carta2=elemento
+        elemento.classList.add("flip")
+        setTimeout(comparaCartas,1000)
+        permissao=false
+        jogadas++
+    }
+}
+
 function inicio(){
-    const numeroCartas=Number(prompt("Deseja jogar com quantas cartas?\n(4, 6, 8, 10, 12 ou 14)"))
     let listaGifs=["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"]
     listaGifs=embaralha(listaGifs)
     let gifsSorteados= listaGifs.splice(0,numeroCartas/2)
@@ -21,11 +61,11 @@ function inicio(){
     for(i=0; i<numeroCartas; i++){
         document.querySelector(".conteiner-cartas").innerHTML+=`
         <div class="carta" onclick="flipar(this)">
-            <div class="face verso">
-                <img src="./gifs/${listaSorteados[i]}"> 
-            </div>
             <div class="face frente">
                 <img src="./imagens/front.png">
+            </div>
+            <div class="face verso">
+                <img src="./gifs/${listaSorteados[i]}"> 
             </div>
         </div>`
     }
